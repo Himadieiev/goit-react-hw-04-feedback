0cb535384e038eb8196f81acdff88b892b41c843
Ruslan Himadieiev <gimadejev86@gmail.com>
2023-05-10 15:38:37 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,55 @@
 import React from 'react';
-import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-import { Section } from "./Section/Section";
-import { Statistics } from "./Statistics/Statistics";
+import { useState } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  handleIncrement = (option) => {
-    this.setState(prevState => {
-      return {
-        ...prevState,
-        [option]: prevState[option] + 1,
-      }
-    })
-  }
+  const handleIncrement = option => {
+    switch (option) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
+  };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
-  }
+  };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
-    return total > 0 ? Math.round(good / total * 100) : 0;
-  }
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions onLeaveFeedback={this.handleIncrement} />
-          <Statistics title="Statistics"
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={positivePercentage}
-          >
-            <Notification message="There is no feedback" />
-          </Statistics>
-        </Section>
-      </div>
-    );
-  }
-};
-
-export default App;
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions onLeaveFeedback={handleIncrement} />
+        <Statistics
+          title="Statistics"
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        >
+          <Notification message="There is no feedback" />
+        </Statistics>
+      </Section>
+    </div>
+  );
+}
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,12 +1,29 @@
-// import PropTypes from 'prop-types';
 import css from './FeedbackOptions.module.css';
 
 export function FeedbackOptions({ onLeaveFeedback }) {
   return (
     <div className={css.buttons}>
-      <button type='button' className={css.button} onClick={() => onLeaveFeedback('good')}>Good</button>
-      <button type='button' className={css.button} onClick={() => onLeaveFeedback('neutral')}>Neutral</button>
-      <button type='button' className={css.button} onClick={() => onLeaveFeedback('bad')}>Bad</button>
+      <button
+        type="button"
+        className={css.button}
+        onClick={() => onLeaveFeedback('good')}
+      >
+        Good
+      </button>
+      <button
+        type="button"
+        className={css.button}
+        onClick={() => onLeaveFeedback('neutral')}
+      >
+        Neutral
+      </button>
+      <button
+        type="button"
+        className={css.button}
+        onClick={() => onLeaveFeedback('bad')}
+      >
+        Bad
+      </button>
     </div>
-  )
+  );
 }
